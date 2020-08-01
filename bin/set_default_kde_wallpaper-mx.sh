bdf04f0dfa6509dcc35f68a80ac095cd275b470d
Dolphin Oracle <dolphinoracle@gmail.com>
2020-08-01 17:13:06 -0400

    add wallpaper setting tool

---
+++ bin/set_default_kde_wallpaper-mx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -e "/usr/share/backgrounds/mx_blue.jpg" ]; then
+	sleep 5
+	qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///usr/share/backgrounds/mx_blue.jpg")}' 
+	if [ "$?" = "0" ]; then 
+		cp /etc/xdg/autostart/set_wallpaper.desktop $HOME/.config/autostart/set_wallpaper.desktop
+		sed -i s/Hidden=.*/Hidden=true/ $HOME/.config/autostart/set_wallpaper.desktop
+	fi
+fi
