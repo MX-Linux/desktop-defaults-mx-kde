fccb3dcec8db23b125e01f913327b05a6247bd17
Adrian <adrian@mxlinux.org>
2023-01-09 22:55:52 -0500

    Remove set_wallpaper script

--- bin/set_default_kde_wallpaper-mx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -e "/usr/share/backgrounds/MX_Tangram.png" ]; then
-	sleep 5
-	qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///usr/share/backgrounds/MX_Tangram.png")}' 
-	if [ "$?" = "0" ]; then 
-		cp /etc/xdg/autostart/set_wallpaper.desktop $HOME/.config/autostart/set_wallpaper.desktop
-		sed -i s/Hidden=.*/Hidden=true/ $HOME/.config/autostart/set_wallpaper.desktop
-	fi
-fi
